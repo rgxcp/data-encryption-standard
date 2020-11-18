2b263f3afe4248b191e68df1c1d0b8f7cdaf86de
Rommy Gustiawan <rgcppp@gmail.com>
2020-11-18 12:00:45 +0700

    Menambahkan note untuk method decrypt

--- javascript/index.js
+++ javascript/index.js
@@ -9,6 +9,7 @@ const key = '12345678';
   console.log(`Encrypted Plain Text in HEX    : ${encryptionInHEX.join(' ')}`);
   console.log(`Encrypted Plain Text in String : ${encryptionInString.join('')}\n`);
 
+  // A wrong key that 90% identical with original key, sometimes can still get decrypted
   const decryptedCipherText = await decrypt(encryptionInBinary, key);
   console.log(`Decrypted Cipher Text : ${decryptedCipherText.join('')}`);
 })();
